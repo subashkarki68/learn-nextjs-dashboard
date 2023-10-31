ba2a12cd4c29ebafa52683598d80961d4ccfb01a
= <=>
2023-10-31 23:39:15 +0545

    Remove: Delay in Data Fetching

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,12 +20,12 @@ export async function fetchRevenue() {
     // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
 
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    // console.log("Fetching revenue data...");
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log("Data fetch complete after 5 seconds.");
+    // console.log("Data fetch complete after 5 seconds.");
 
     return data.rows;
   } catch (error) {
